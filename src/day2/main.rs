c90ef37fc3a980e0b94575f56e09a3c1f5b42e8d
Gimbaro <me@gimbaro.dev>
2024-12-02 08:56:38 +0100

    :sparkles: day2 problem 2

--- src/day2/main.rs
+++ src/day2/main.rs
@@ -3,29 +3,51 @@
 fn main() {
   let contents = include_str!("input.txt");
   print!("{}\n", problem1(contents.to_string()));
-  //print!("{}\n", problem2(contents.to_string()));
+  print!("{}\n", problem2(contents.to_string()));
 }
 
 fn problem1(contents: String) -> i32 {
     let mut count = 0;
     for line in contents.lines() {
-        if is_incr_or_decr(line.to_string()) && elements_differentiate_by(line.to_string(), 3) {
-            count = count+1;
+        let arr_line: Vec<i32> = line.split_ascii_whitespace().map(|el| el.parse::<i32>().unwrap()).collect();
+        if is_incr_or_decr(arr_line.clone()) && elements_differentiate_by(arr_line.clone(), 3) {
+            count += 1;
         }
     }
     return count;
 }
 
-fn is_incr_or_decr(line: String) -> bool {
-    let splitted = line.split(" ").map(|el| el.parse::<i32>().unwrap());
-    return splitted.clone().is_sorted_by(|a, b| a > b) || splitted.clone().is_sorted_by(|a, b| a < b)
+fn problem2(contents: String) -> i32 {
+    let mut count = 0;
+    for line in contents.lines() {
+        let arr_line: Vec<i32> = line.split_ascii_whitespace().map(|el| el.parse::<i32>().unwrap()).collect();
+        if is_line_ok(arr_line) {
+            count += 1
+        }
+    }
+    return count;
 }
 
-fn elements_differentiate_by(line: String, by: i32) -> bool {
-    let mut splitted = line.split_ascii_whitespace();
+fn is_line_ok(line: Vec<i32>) -> bool {
+    for n in 0..line.len() {
+        let mut new_line = line.clone();
+        new_line.remove(n);
+        if is_incr_or_decr(new_line.clone()) && elements_differentiate_by(new_line.clone(), 3) {
+            return true
+        }
+    }
+    return false
+}
+
+fn is_incr_or_decr(line: Vec<i32>) -> bool {
+    return line.clone().is_sorted_by(|a, b| a > b) || line.clone().is_sorted_by(|a, b| a < b)
+}
+
+fn elements_differentiate_by(line: Vec<i32>, by: i32) -> bool {
+    let mut splitted = line.iter();
     let mut prev = splitted.next().unwrap();
     for elem in splitted {
-        let difference = (prev.parse::<i32>().unwrap() - elem.parse::<i32>().unwrap()).abs();
+        let difference = (prev - elem).abs();
         prev = elem;
         if difference > by {
             return false
