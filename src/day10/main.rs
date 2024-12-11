674df939af2fa668e43602fdf4fddd075027fdf4
Elia Gamberi <me@gimbaro.dev>
2024-12-11 16:20:39 +0100

    day 10

---
+++ src/day10/main.rs
@@ -0,0 +1,92 @@
+
+
+const OUTSIDE: char = '#';
+
+fn main() {
+  let contents = include_str!("input.txt");
+  //print!("{}\n", problem1(contents.to_string()));
+  print!("{}\n", problem2(contents.to_string()));
+}
+
+fn problem1(contents: String) -> u32 {
+  let mut count = 0;
+  
+  let mut formatted_content: Vec<Vec<u32>> = surround_content(contents);
+  
+  for (y, line) in formatted_content.iter().enumerate() {
+    for (x, character) in line.iter().enumerate() {
+      if *character == 0 {
+        count += count_n_trailheads(0, x, y, &formatted_content).len();
+      }
+    }
+  }
+  
+  return count as u32;
+}
+
+fn problem2(contents: String) -> u32 {
+  let mut count = 0;
+  
+  let mut formatted_content: Vec<Vec<u32>> = surround_content(contents);
+  
+  for (y, line) in formatted_content.iter().enumerate() {
+    for (x, character) in line.iter().enumerate() {
+      if *character == 0 {
+        count += count_paths(0, x, y, &formatted_content);
+      }
+    }
+  }
+  
+  return count as u32;
+}
+
+fn count_paths(value: u32, x: usize, y: usize, table: &Vec<Vec<u32>>) -> u32 {
+  let line_len = &table.len();
+  let target = value+1;
+  let mut count = 0;
+  print!("{}, {} - searching {} \n", x, y, target);
+  if target == 10 { return 1 }
+  if x>0 && table[y][x-1] == target { count += count_paths(target, x-1, y, &table) }
+  if x<line_len-1 && table[y][x+1] == target { count += count_paths(target, x+1, y, &table) }
+  if y>0 && table[y-1][x] == target { count += count_paths(target, x, y-1, &table) }
+  if y<line_len-1 && table[y+1][x] == target { count += count_paths(target, x, y+1, &table) }
+
+  return count
+}
+
+fn count_n_trailheads(value: u32, x: usize, y: usize, table: &Vec<Vec<u32>>) -> Vec<(usize, usize)> {
+  let line_len = &table.len();
+  let target = value+1;
+  print!("{}, {} - searching {} \n", x, y, target);
+  let mut to_return: Vec<(usize, usize)> = vec![];
+  if target == 10 { return vec![(x, y)]; }
+  if x>0 && table[y][x-1] == target {
+    to_return.append(&mut count_n_trailheads(target, x-1, y, &table));
+  }
+  if x<line_len-1 && table[y][x+1] == target {
+    to_return.append(&mut count_n_trailheads(target, x+1, y, &table))
+  }
+  if y>0 && table[y-1][x] == target {
+    to_return.append(&mut count_n_trailheads(target, x, y-1, &table))
+  }
+  if y<line_len-1 && table[y+1][x] == target {
+    to_return.append(&mut count_n_trailheads(target, x, y+1, &table))
+  }
+
+  to_return.sort();
+  to_return.dedup();
+  return to_return
+}
+
+fn surround_content(contents: String) -> Vec<Vec<u32>> {
+  let line_len = contents.lines().next().unwrap().len()+2;
+  let only_surround_line: Vec<char> = (0..line_len).map(|_| OUTSIDE).collect();
+
+  let mut final_content: Vec<Vec<u32>> = Vec::new();
+  //final_content.push(only_surround_line.clone());
+  for line in contents.lines() {
+    final_content.push(format!("{line}").chars().map(|c| if let Some(value) = c.to_digit(10) { return value } else { return 100; } ).collect());
+  }
+  //final_content.push(only_surround_line);
+  return final_content;
+}
