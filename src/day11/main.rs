6124b4ad1a2d1bcb9563a790a0151b93877f4057
Gimbaro <me@gimbaro.dev>
2024-12-12 00:51:18 +0000

    cleaned up code

--- src/day11/main.rs
+++ src/day11/main.rs
@@ -1,9 +1,13 @@
-use std::{collections::HashMap, hash::Hash};
+use std::collections::HashMap;
 
 fn main() {
   let contents = include_str!("input.txt");
   print!("{}\n", problem(contents.to_string()));
 }
+
+// for problem 1 just change this constant to 25
+
+const NSTEPS: usize = 75;
 
 fn problem(contents: String) -> u64 {
   let mut stones: Vec<String> = vec![];
@@ -11,40 +15,17 @@ fn problem(contents: String) -> u64 {
     stones = line.split(" ").collect::<Vec<&str>>().iter().map(|&s| s.to_string()).collect();
   }
 
-  print!("{:?}\n", stones);
-
   let mut count = 0;
   let mut cache_results: HashMap<(String, usize), u64> = HashMap::new();
 
   for stone in stones {
     print!("facendo sasso {}\n", stone);
-    count += rock_and_stone(&stone, 75, &mut cache_results);
-    print!("partial count: {}\n", count);
+    count += rock_and_stone(&stone, NSTEPS, &mut cache_results);
   }
   return count;
 }
 
-fn blink(stones: Vec<String>) -> Vec<String> {
-  let mut new_stones: Vec<String> = vec![];
-  stones.iter().enumerate().for_each( |(id, stone)| {
-    if *stone == "0" {
-      new_stones.push("1".to_string());
-      return;
-    }
-    if stone.len() % 2 == 0 {
-      let mid = stone.char_indices().count() / 2;
-      let (first, second) = stone.split_at(mid);
-      new_stones.push(format!("{}", first.parse::<i32>().unwrap()));
-      new_stones.push(format!("{}", second.parse::<i32>().unwrap()));
-      return;
-    }
-    new_stones.push( format!("{}", stone.parse::<u64>().unwrap() * 2024));
-  });
-  return new_stones;
-}
-
 fn rock_and_stone(stone: &str, missing_steps: usize, cache: &mut HashMap<(String, usize), u64>) -> u64 {
-  //if missing_steps == 1 { print!("{} ", stone); }
   if missing_steps <= 0 { return 1; }
   match cache.get(&(stone.to_string(), missing_steps)) {
     Some(stone_result) => {return *stone_result;},
